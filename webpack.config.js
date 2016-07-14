06d91c47e49bdcb94d8948026defc41ac83fb663
Nguyen Trong Nghia <ntngh2712@gmail.com>
2016-07-14 19:17:41 +0700

    Fix webpack static file issues.

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = [
       path: './DanceCat/static/bundle/',
       filename: 'DanceCat.[name].js',
       library: ['DanceCat', "[name]"],
-      libraryTarget: "umd"
+      libraryTarget: "umd",
+      publicPath: '/static/bundle/'
     },
     module: {
       loaders: [
@@ -38,7 +39,7 @@ module.exports = [
         },
         {
           test: /\.(eot|ttf|svg|gif|png|woff|woff2)$/,
-          loader: "file?name=/static/bundle/[hash].[ext]"
+          loader: "file?name=[hash].[ext]"
         },
         {
           test: /\.js$/,
